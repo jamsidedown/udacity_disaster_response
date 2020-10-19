9a5f5d26424dce6589238aa89604f193bcfb88c8
jamsidedown <robertanderson1993@googlemail.com>
2020-10-19 11:51:50 +0100

    Added docstring to process_data

--- src/process_data.py
+++ src/process_data.py
@@ -6,6 +6,19 @@ from .utils import MESSAGES_PATH, CATEGORIES_PATH, DATABASE_PATH, save_dataframe
 
 
 def load_data(messages_path: str, categories_path: str) -> pd.DataFrame:
+    '''
+    load_data
+        Load messages and categories from csv files, return the combined pandas dataframe
+
+    args:
+        messages_path: str
+            The path of the messages csv
+        categories_path: str
+            The path of the categories csv
+
+    returns:
+        pandas.DataFrame containing merged messages and categories data
+    '''
     messages = pd.read_csv(messages_path)
     categories = pd.read_csv(categories_path)
 
@@ -13,6 +26,17 @@ def load_data(messages_path: str, categories_path: str) -> pd.DataFrame:
 
 
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    clean_data
+        Split categories into multiple columns and convert to binary with duplicates removed
+
+    args:
+        df: pandas.DataFrame
+            The dataframe to clean
+
+    returns:
+        pandas.DataFrame containing cleaned dataset
+    '''
     categories_df = df['categories'].str.split(';', expand=True)
     category_headers = categories_df.iloc[0].apply(lambda x: x[:-2])
     categories_df.columns = category_headers
@@ -28,6 +52,19 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
 
 def main(messages_path: str = MESSAGES_PATH, categories_path: str = CATEGORIES_PATH,
          database_path: str = DATABASE_PATH) -> None:
+    '''
+    main
+        Load data from messages and categories csv files
+        Clean the data and save it in to the database path
+
+    args:
+        messages_path: str (default to MESSAGES_PATH)
+        categories_path: str (default to CATEGORIES_PATH)
+        database_path: str (default to DATABASE_PATH)
+
+    returns:
+        None
+    '''
     print('==== Loading data ====')
     print(f'MESSAGES: {messages_path}')
     print(f'CATEGORIES: {categories_path}')
